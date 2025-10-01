2411cebb80a6589d97701a3ecd65a04a8b005b23
Shriyaa Sooklal <sklshr001@myuct.ac.za>
2025-10-01 18:11:17 +0200

    new changes

--- multistate.R
+++ multistate.R
@@ -18,7 +18,7 @@ ddl$Psi$fix <- with(ddl$Psi, ifelse(stratum == "2" & tostratum == "1", 0, NA))
 model.list <- list(
   S = list(formula = ~stratum * time),
   p = list(formula = ~stratum * time),
-  Psi = list(formula = ~stratum * time)
+  Psi = list(formula = ~ time)
 )
 
 # 6. Fit the model
@@ -33,7 +33,8 @@ fit.constrained <- mark(
 summary(fit.constrained)
 
 # save results
-install.packages("writexl")  # Only once
+real_estimates = fit.constrained$results$real
+real_estimates <- data.frame(Parameter=rownames(real_estimates), real_estimates)
 library(writexl)
 write_xlsx(real_estimates, "real_estimates.xlsx")
 
@@ -61,28 +62,27 @@ ddl$Psi$ageclass <- cut(ddl$Psi$Age,
                         labels = c("1","2","3","4"), right = FALSE)
 
 # ---- 6. Model formula list
-model.list <- list(
+model.list1 <- list(
   S = list(formula = ~ stratum + time),         # Survival: state + time
   p = list(formula = ~ stratum + time),         # Recapture: state + time
   Psi = list(formula = ~ time + ageclass)       # Transition: time + age
 )
 
 # ---- 7. Fit the model
-fit <- mark(ms.processed, ddl, model.parameters = model.list)
+fit1 <- mark(ms.processed, ddl, model.parameters = model.list1)
 
 # ---- 8. View results
-summary(fit)
+summary(fit1)
 
-### design matrix for specific params
-# For survival (S)
-head(ddl$S)
+# save results
+real_estimates1 = fit1$results$real
+real_estimates1 <- data.frame(Parameter=rownames(real_estimates1), real_estimates1)
+library(writexl)
+write_xlsx(real_estimates1, "real_estimates1.xlsx")
 
-# For recapture (p)
-head(ddl$p)
 
-# For transition probabilities (Psi)
-head(ddl$Psi)
-#####constant model#####
+
+#####baseline model#####
 library(RMark)
 
 # Load data
@@ -97,30 +97,21 @@ ddl$Psi$fix[ddl$Psi$stratum=="2" & ddl$Psi$tostratum=="1"] <- 0
 
 # Model
 model.const <- list(
-  S   = list(formula = ~1),
-  p   = list(formula = ~1),
+  S   = list(formula = ~stratum),
+  p   = list(formula = ~stratum),
   Psi = list(formula = ~1)
 )
 fit.const <- mark(ms.processed, ddl, model.parameters = model.const)
-fit.const$results$AICc
+
+# save results
+real_estimates2 = fit.const$results$real
+real_estimates2 <- data.frame(Parameter=rownames(real_estimates2), real_estimates2)
+library(writexl)
+write_xlsx(real_estimates2, "real_estimates2.xlsx")
 
 
-#####S.P.PSIt#####
-library(RMark)
 
-data <- convert.inp("peregrine_multistate_final.inp", group.df = NULL)
-ms.processed <- process.data(data, model = "Multistrata", strata.labels = c("1","2"))
-ddl <- make.design.data(ms.processed)
-ddl$Psi$fix[ddl$Psi$stratum=="2" & ddl$Psi$tostratum=="1"] <- 0
-
-model.Psit <- list(
-  S   = list(formula = ~1),
-  p   = list(formula = ~1),
-  Psi = list(formula = ~time)
-)
-fit.Psit <- mark(ms.processed, ddl, model.parameters = model.Psit)
-fit.Psit$results$AICc
-#####age model#####
+#####simpler additive model-only psi has age effect#####
 library(RMark)
 
 data <- convert.inp("peregrine_multistate_final.inp", group.df = NULL)
@@ -132,18 +123,24 @@ ddl$Psi$fix[ddl$Psi$stratum=="2" & ddl$Psi$tostratum=="1"] <- 0
 
 # Create age classes for NB→B transitions
 ddl$Psi$ageclass <- cut(ddl$Psi$Age,
-                        breaks = c(0,2,3,4,6,100),
-                        labels = c("1","2","3","4","5"),
+                        breaks = c(0,2,3,4,100),
+                        labels = c("1","2","3","4"),
                         right = FALSE)
 
 # Model
 model.Psiage <- list(
-  S   = list(formula = ~stratum),
-  p   = list(formula = ~1),
+  S   = list(formula = ~stratum + time),
+  p   = list(formula = ~stratum + time),
   Psi = list(formula = ~ageclass)
 )
 fit.Psiage <- mark(ms.processed, ddl, model.parameters = model.Psiage)
-fit.Psiage$results$AICc
+
+# save results
+real_estimates3 = fit.Psiage$results$real
+real_estimates3 <- data.frame(Parameter=rownames(real_estimates3), real_estimates3)
+library(writexl)
+write_xlsx(real_estimates3, "real_estimates3.xlsx")
+
 #####survival->age class for nonbreeders####
 library(RMark)
 
@@ -169,10 +166,63 @@ ddl$Psi$ageclass <- cut(ddl$Psi$Age,
 
 # Model: NB survival depends on ageclass2, B survival varies by time
 model.ageNB <- list(
-  S   = list(formula = ~stratum + I(stratum=="1")*ageclass2 + I(stratum=="2")*time),
+  S   = list(formula = ~ I(stratum=="1")*ageclass2 + I(stratum=="2")*time),
   p   = list(formula = ~stratum + time),
   Psi = list(formula = ~ageclass)
 )
 
 fit.ageNB <- mark(ms.processed, ddl, model.parameters = model.ageNB)
-fit.ageNB$results$AICc
+
+# ---- 8. View results
+summary(fit.ageNB)
+
+# save results
+real_estimatesNB = fit.ageNB$results$real
+real_estimatesNB <- data.frame(Parameter=rownames(real_estimatesNB), real_estimatesNB)
+library(writexl)
+write_xlsx(real_estimatesNB, "real_estimatesNB.xlsx")
+
+##### survival varying for nonbreeder and constant for breeders#####
+library(RMark)
+
+data <- convert.inp("peregrine_multistate_final.inp", group.df = NULL)
+ms.processed <- process.data(data, model = "Multistrata", strata.labels = c("1","2"))
+ddl <- make.design.data(ms.processed)
+
+# Fix breeder → nonbreeder to 0
+ddl$Psi$fix[ddl$Psi$stratum=="2" & ddl$Psi$tostratum=="1"] <- 0
+
+# NB survival: 2 age classes
+ddl$S$ageclass2 <- cut(ddl$S$Age,
+                       breaks = c(0,2,100),
+                       labels = c("young","older"),
+                       right = FALSE)
+
+# NB→B transitions: 4 age classes
+ddl$Psi$ageclass <- cut(ddl$Psi$Age,
+                        breaks = c(0,2,3,4,100),
+                        labels = c("1","2","3","4"),
+                        right = FALSE)
+
+
+# Model: NB survival depends on ageclass2, B survival varies by time
+model.ageNB2 <- list(
+  S   = list(formula = ~I(stratum=="1")*(ageclass2 + time) + I(stratum=="2")*time),
+  p   = list(formula = ~stratum + time),
+  Psi = list(formula = ~ageclass)
+)
+
+fit.ageNB2 <- mark(ms.processed, ddl, model.parameters = model.ageNB2)
+
+# save results
+real_estimatesNB2 = fit.ageNB2$results$real
+real_estimatesNB2 <- data.frame(Parameter=rownames(real_estimatesNB2), real_estimatesNB2)
+library(writexl)
+write_xlsx(real_estimatesNB2, "real_estimatesNB2.xlsx")
+
+#####AICs for the above 6 models#####
+all.models <- collect.models()
+aic.table <- all.models$model.table
+
+write_xlsx(aic.table, "AICc_results.xlsx")
+
